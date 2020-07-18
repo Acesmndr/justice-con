6a9c4278e180249ba2df19f7f8536d6b7d9e6f10
acesmndr <acesmndr@gmail.com>
2020-07-18 20:15:22 +0545

    Fix the timezone issue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Countdown forDate="Sat, 25 Jul 2020 14:45:00 GMT" />
+      <Countdown forDate="Sat, 25 Jul 2020 15:30:00 GMT+0100" />
       <div className="footer">Made with love by <a className="developer" href="https://twitter.com/xsmndr">xsmndr</a></div>
     </div>
   );
