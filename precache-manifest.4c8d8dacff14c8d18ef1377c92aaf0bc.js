881d959ef9de7b9962987227fea3d86244df3b09
acesmndr <acesmndr@gmail.com>
2020-07-18 18:49:44 +0545

    Updates

---
+++ precache-manifest.4c8d8dacff14c8d18ef1377c92aaf0bc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7b8ca02364b824f8100f2f64c664cf",
+    "url": "/justice-con/index.html"
+  },
+  {
+    "revision": "4fc26e0ddc993e776dc2",
+    "url": "/justice-con/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "fb76d1e89e107819ffb7",
+    "url": "/justice-con/static/css/main.9a9aee67.chunk.css"
+  },
+  {
+    "revision": "4fc26e0ddc993e776dc2",
+    "url": "/justice-con/static/js/2.d79b5ba7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/justice-con/static/js/2.d79b5ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb76d1e89e107819ffb7",
+    "url": "/justice-con/static/js/main.a8c36f01.chunk.js"
+  },
+  {
+    "revision": "14c8f7c69920d686744d",
+    "url": "/justice-con/static/js/runtime-main.a8c7715a.js"
+  },
+  {
+    "revision": "130d2d28847f29509c862291e79191b7",
+    "url": "/justice-con/static/media/icon.130d2d28.png"
+  },
+  {
+    "revision": "4c236db629f2520cfd6763a50c646f1e",
+    "url": "/justice-con/static/media/justicecon.4c236db6.png"
+  }
+]);
