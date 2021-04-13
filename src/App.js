7d28f31fa59d3b8c2fef75de5392bbc3dc72a784
acesmndr <acesmndr@gmail.com>
2021-04-13 18:32:23 +0545

    Added credits

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           <Route exact path='/' render={(routeProps) => <Countdown forDate="Fri, 16 Apr 2021 06:00:00 GMT-0400" toDate="Sun, 18 Apr 2021 19:30:00 GMT-0400" {...routeProps} />} />
           <Route render={(routeProps) => <Countdown forDate="Fri, 16 Apr 2021 06:00:00 GMT-0400" toDate="Sun, 18 Apr 2021 19:30:00 GMT-0400" {...routeProps} />} />
         </Switch>
-        <div className="footer">Made with love by <a className="developer" href="https://twitter.com/xsmndr">xsmndr</a></div>
+        <div className="footer">Made with ❤ by <a className="developer" href="https://twitter.com/xsmndr">acesmndr</a><a className="developer-card" href="https://about.me/acesmndr"><img class="ace-of-spades" src={require('./aces.jpeg')} alt="card" /></a></div>
         <ReactTooltip />
       </HashRouter>
     </div>
